5e1a01344a00d4126dd1f3a2a0d98131b5d0a7e6
Sachin Chakrawarti <sachinchakrawarti69@gmail.com>
2026-07-23 09:37:15 +0530

    [#9] 📦 Update 3 configuration

--- projects/ethereum/api/historical/database/queries.js
+++ projects/ethereum/api/historical/database/queries.js
@@ -1,1 +1,61 @@
+/**
+ * SQL Queries
+ */
 
+export const INSERT_MARKET = `
+INSERT OR REPLACE INTO historical_market (
+
+    date,
+    symbol,
+
+    open,
+    high,
+    low,
+    close,
+
+    volume_eth,
+    volume_usdt,
+
+    number_of_trades,
+
+    taker_buy_volume_eth,
+    taker_buy_volume_usdt,
+
+    market_cap_usd,
+    total_volume_usd,
+
+    circulating_supply,
+    total_supply,
+    max_supply,
+
+    market_cap_rank
+
+)
+
+VALUES (
+
+?,?,?,?,?,
+?,
+?,?,?,
+?,
+?,
+?,
+?,
+?,
+?,
+?,
+?
+
+);
+`;
+
+export const SELECT_ALL = `
+SELECT *
+FROM historical_market
+ORDER BY date;
+`;
+
+export const SELECT_LAST_DATE = `
+SELECT MAX(date) AS last_date
+FROM historical_market;
+`;
