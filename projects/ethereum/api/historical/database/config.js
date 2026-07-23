2d5480b20805565e87d00b8766955c4180f70e10
Sachin Chakrawarti <sachinchakrawarti69@gmail.com>
2026-07-23 10:12:16 +0530

    [#47] 📦 Update 3 configuration

--- projects/ethereum/api/historical/database/config.js
+++ projects/ethereum/api/historical/database/config.js
@@ -3,12 +3,33 @@
  */
 
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
+/*
+Project Structure
+
+ethereum/
+│
+├── database/
+│   └── ethereum.db
+│
+└── api/
+    └── historical/
+        └── database/
+            └── config.js
+*/
+
+const DATABASE_PATH = path.resolve(
+    __dirname,
+    "../../../database/ethereum.db"
+);
 
 export default {
 
-    database: path.resolve(
-        "database",
-        "ethereum.db"
-    )
+    DATABASE_PATH
 
 };
