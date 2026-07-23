0fc9797769208113747473aa501a8978bb3f85d8
Sachin Chakrawarti <sachinchakrawarti69@gmail.com>
2026-07-23 10:28:02 +0530

    [#74] 📦 Update 3 configuration

---
+++ projects/ethereum/api/historical/scheduler/index.js
@@ -0,0 +1,27 @@
+/**
+ * Historical Scheduler
+ */
+
+import backfill from "./backfill.js";
+import dailySync from "./daily_sync.js";
+import startScheduler from "./cron.js";
+
+export {
+
+    backfill,
+
+    dailySync,
+
+    startScheduler
+
+};
+
+export default {
+
+    backfill,
+
+    dailySync,
+
+    startScheduler
+
+};
