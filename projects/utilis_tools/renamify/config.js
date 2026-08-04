6e333fe67f805208b9aaa0a097daf6d53cf4b7d6
Sachin Chakrawarti <chakrawartisachin012@gmail.com>
2026-08-05 00:19:12 +0530

    [#1] ⚡ Update 2 JavaScript code

--- projects/utilis_tools/renamify/config.js
+++ projects/utilis_tools/renamify/config.js
@@ -5,19 +5,19 @@ const path = require('path');
 module.exports = {
     // Folder where images are stored
     ASSETS_PATH: path.join(__dirname, 'assets'),
-    
+
     // Base name for renamed files
-    BASE_NAME: 'akanksha_puri',
-    
+    BASE_NAME: 'anjali_negi',
+
     // Supported image formats
     IMAGE_EXTENSIONS: ['.jpg', '.jpeg', '.png', '.gif', '.webp', '.bmp', '.tiff', '.svg'],
-    
+
     // Number padding (6 = 000001, 4 = 0001, etc.)
     PADDING: 6,
-    
+
     // If true, only preview changes (no actual renaming)
     DRY_RUN: false,
-    
+
     // If true, show detailed logs
     VERBOSE: true
 };
